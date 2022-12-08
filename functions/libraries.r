9d9d6e66d9d4cf3765ff95eb351a4c2e5d711114
Frogboi24 <117742865+Frogboi24@users.noreply.github.com>
2022-12-08 19:40:43 +0000

    Create libraries.r

---
+++ functions/libraries.r
@@ -0,0 +1,9 @@
+# libraries required for penguin_assignment.R
+
+library(palmerpenguins)
+library(ggplot2)
+suppressPackageStartupMessages(library(janitor))
+suppressPackageStartupMessages(library(dplyr))
+library(tidyr)
+library(ragg)
+library(svglite)
